7b735b88898a46ab0554f5105a0e2ce75c9c5f3c
NDH <nick@ndhrones.com>
2023-09-24 23:26:38 -0400

    first commit

---
+++ deps.ts
@@ -0,0 +1,7 @@
+export { serve } from "https://deno.land/std@0.201.0/http/server.ts";
+export { join } from "https://deno.land/std@0.201.0/path/mod.ts";
+export * from "https://deno.land/std@0.201.0/http/file_server.ts"
+export { debounce } from "https://deno.land/std@0.201.0/async/debounce.ts";
+export * as esbuild from "https://deno.land/x/esbuild@v0.17.11/mod.js";
+export { denoPlugin } from "https://deno.land/x/esbuild_deno_loader@0.6.0/mod.ts";
+export * from "https://raw.githubusercontent.com/nhrones/Browser/master/browser.ts"
